938ebed86aed7b1fece13940c3174ff8998cd2df
superpepe2003 <pablo_fretes@hotmail.com>
2018-09-03 21:09:31 -0300

    Modulo menu

--- app/src/main/java/com/example/usuario/geoubicacion/inicialActivity.java
+++ app/src/main/java/com/example/usuario/geoubicacion/inicialActivity.java
@@ -1,9 +1,11 @@
 package com.example.usuario.geoubicacion;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class inicialActivity extends AppCompatActivity {
 
@@ -14,8 +16,22 @@ public class inicialActivity extends AppCompatActivity {
     }
 
     @Override
-    public boolean onCreateOpptionsMenu(Menu menu)   {
+    public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menuprincipal, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem opcMenu) {
+        int id = opcMenu.getItemId();
+        if (id == R.id.menu_admin) {
+            Intent in = new Intent(this, MainActivity.class);
+            startActivity(in);
+        }
+        if (id == R.id.menu_user) {
+            Intent in = new Intent(this, userActivity.class);
+            startActivity(in);
+        }
         return true;
     }
 
